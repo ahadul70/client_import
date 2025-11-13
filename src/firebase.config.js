f75e425af3a21aa5ca3b1528a3a6fc21ecd05b38
ahaul <ahadul70@gmail.com>
2025-11-13 21:30:31 +0600

    feat: Update Firebase configuration to use environment variables and enhance .gitignore for security

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -3,12 +3,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAae64KJ7qSF9WIC_EQAuqHezxY9HC5pHA",
-  authDomain: "assignment-9-c42cc.firebaseapp.com",
-  projectId: "assignment-9-c42cc",
-  storageBucket: "assignment-9-c42cc.appspot.com",
-  messagingSenderId: "733257952765",
-  appId: "1:733257952765:web:a39e7322ff62ef2176fd94",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.emv.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.emv.VITE_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
